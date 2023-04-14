fce6e77a92d70938dd0c3ed593b84da3eb72c01f
Felipe Capella <felipecapellar6@gmail.com>
2023-04-14 19:22:26 -0300

    enviando pro repositorio

---
+++ config.ini.php
@@ -0,0 +1,11 @@
+<?php
+    $config = [
+                "server" =>  "localhost", 
+                "dbname" => "ponto_eletronico", 
+                "user" => "root", 
+                "password" => "",
+                "dbtype" => "mysql",
+                "port" => 3307,
+                "client_name" => "Empresa A"
+    ];
+?>
